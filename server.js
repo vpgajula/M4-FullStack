e9401fd0c04937d4594dc9f82e3043e4226ffd6f
Venkateswara Prasad Gajula <82006544+vpgajula@users.noreply.github.com>
2023-02-25 12:55:17 -0500

    Added config.env file

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+
+const dotenv = require('dotenv'); 
+dotenv.config({ path: './config.env' }); // this line has to come first since we need to tell where is the config file
 const express = require('express');
 const app = express();
+
+console.log(process.env.ATLAS_USERNAME)
+
+const morgan = require('morgan');
+//const app = require('./app'); 
 const port = 3000;
+
+if (process.env.NODE_ENV === 'development') {
+    //app.use(morgan('combined :method :url :status :res[content-length] - :response-time ms'))
+    app.use(morgan('combined'))
+}
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -27,7 +40,12 @@ const mongoose = require('mongoose');
 // });
 
 //asynchronous DB connection
-mongoose.connect('mongodb+srv://vpgajula:iftpassword@myfirstmongocluster-gvp.o8zigu8.mongodb.net/IFT-598-2023', {useNewUrlParser: true})
+//mongoose.connect('mongodb+srv://vpgajula:iftpassword@myfirstmongocluster-gvp.o8zigu8.mongodb.net/IFT-598-2023', {useNewUrlParser: true})
+
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@${process.env.ATLAS_DBSERVER}/${process.env.DATABASE}`
+,{useNewUrlParser: true})
+
+
     .then(() => console.log('MongoDB connection succeeded.'))
     .catch((err) => console.log('Error in DB connection: ' + err));
 
